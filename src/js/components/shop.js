f6a01a1af4666b40d88aaa486abfe564913c45d7
PhanTrongPhuc <phantrongphuc12004@gmail.com>
2024-11-02 15:02:27 +0700

    update data and cofig to open index file by nomal way, little in shop page

--- src/js/components/shop.js
+++ src/js/components/shop.js
@@ -8,12 +8,13 @@ $(document).ready(function () {
         // Duyệt qua từng sản phẩm và tạo thẻ card
         $.each(data, function (index, product) {
             const cardHtml = `
-                <div class="col-md-4">  <div class="card">
+                <div class="col-md-4" data-product-id="${product.id}">  <div class="card">
                     <img src="../../../${product.anhBia}" class="card-img-top" alt="${product.ten}">
                     <div class="card-body">
                         <h5 class="card-title">${product.ten}</h5>
                         <p class="card-text">${product.moTa}</p>
                         <p class="card-text">Giá: ${product.gia} VNĐ</p>
+                        <button class="btn btn-primary">Mua</button> </div>
                     </div>
                     </div>
                 </div>
@@ -21,4 +22,14 @@ $(document).ready(function () {
             $container.append(cardHtml);
         });
     });
+});
+
+// Bắt sự kiện click vào toàn bộ phần tử có class "col-md-4"
+$$('.col-md-4').on('click', function() {
+    const productId = $(this).data('product-id');
+    // Lưu productId vào Local Storage
+    localStorage.setItem('productId', productId);
+
+    // Chuyển hướng đến trang chi tiết
+    window.location.href = './detail.html';
 });
--- src/js/index.js
+++ src/js/index.js
@@ -1,52 +1,31 @@
 // ĐÂY LÀ FILE JS CHO TRANG CHỦ
 
-//Lấy dữ liệu từ file header.html rồi đưa vào thẻ có id="header"
-fetch('/src/components/common/header.html')
-    .then(response => response.text())
-    .then(data => {
-        // Tạo một phần tử div tạm thời để chèn nội dung
-        const tempDiv = document.createElement('div');
-        tempDiv.innerHTML = data;
+// Chèn phần header từ header.js vào trang chủ
+const headerElement = document.getElementById('header');
+headerElement.innerHTML = headerHTML;
 
-        // Tìm và thay thế thẻ base nếu có
-        const baseElement = tempDiv.querySelector('base');
-        if (baseElement) {
-            // Điều chỉnh href của thẻ base nếu cần
-            baseElement.href = '/'; // Ví dụ: đặt đường dẫn cơ sở là gốc
-        }
-        const headerElement = document.getElementById('header');
-        headerElement.innerHTML = tempDiv.innerHTML;
-    });
+// Chèn phần footer từ footer.js vào trang chủ
+const footerElement = document.getElementById('footer');
+footerElement.innerHTML = footerHTML;
 
-//Lấy dữ liệu từ file footer.html rồi đưa vào thẻ có id="footer"
-fetch('/src/components/common/footer.html')
-    .then(response => response.text())
-    .then(data => {
-        const footerElement = document.getElementById('footer');
-        footerElement.innerHTML = data;
-    });
+// Lấy dữ liệu từ file products.js vào
+const container = document.getElementById('product-container');
+products.forEach(product => {
+    const card = document.createElement('div');
+    card.classList.add('col-md-4');
 
-// Hiển thị sản phẩm từ dữ liệu JSON dùng jQuery
-$(document).ready(function () {
-    // Lấy dữ liệu từ file JSON
-    $.getJSON('/Data/Location.json', function (data) {
-        // Chọn phần tử để hiển thị các card
-        const $container = $('#product-container');
+    const cardContent = `
+      <div class="card">
+        <img src="./${product.anhBia}" class="card-img-top" alt="${product.ten}">
+        <div class="card-body">
+          <h5 class="card-title">${product.ten}</h5>
+          <p class="card-text">${product.moTa}</p>
+          <p class="card-text">Giá: ${product.gia} VNĐ</p>
+          <button class="btn btn-primary">Xem chi tiết</button>
+        </div>
+      </div>
+    `;
 
-        // Duyệt qua từng sản phẩm và tạo thẻ card
-        $.each(data, function (index, product) {
-            const cardHtml = `
-                <div class="col-md-4">  <div class="card">
-                    <img src="${product.anhBia}" class="card-img-top" alt="${product.ten}">
-                    <div class="card-body">
-                        <h5 class="card-title">${product.ten}</h5>
-                        <p class="card-text">${product.moTa}</p>
-                        <p class="card-text">Giá: ${product.gia} VNĐ</p>
-                    </div>
-                    </div>
-                </div>
-            `;
-            $container.append(cardHtml);
-        });
-    });
+    card.innerHTML = cardContent;
+    container.appendChild(card);
 });
