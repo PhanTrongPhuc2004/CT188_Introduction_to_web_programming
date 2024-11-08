b22bce2088c0d1354b4a9532a83ec24261f01b72
PhanTrongPhuc <phantrongphuc12004@gmail.com>
2024-11-08 15:06:19 +0700

    update js for shop pages

--- src/js/components/shop.js
+++ src/js/components/shop.js
@@ -1,11 +1,11 @@
 // Lấy dữ liệu từ file products.js vào
 const container = document.getElementById('product-container');
 products.forEach(product => {
-    const card = document.createElement('div');
-    card.classList.add('col-md-4');
+  const card = document.createElement('div');
+  card.classList.add('col-md-4');
 
-    const cardContent = `
-      <div class="card">
+  const cardContent = `
+      <div class="card" data-product-id="${product.id}">
         <img src="./${product.anhBia}" class="card-img-top" alt="${product.ten}">
         <div class="card-body">
           <h5 class="card-title">${product.ten}</h5>
@@ -15,17 +15,25 @@ products.forEach(product => {
         </div>
       </div>
     `;
-
-    card.innerHTML = cardContent;
-    container.appendChild(card);
+  console.log(product.id);
+  card.innerHTML = cardContent;
+  container.appendChild(card);
 });
 
-// // Bắt sự kiện click vào toàn bộ phần tử có class "col-md-4"
-// $$('.col-md-4').on('click', function() {
-//     const productId = $(this).data('product-id');
-//     // Lưu productId vào Local Storage
-//     localStorage.setItem('productId', productId);
+// Lấy tất cả các phần tử có class "col-md-4"
+const elements = document.querySelectorAll('.card');
 
-//     // Chuyển hướng đến trang chi tiết
-//     window.location.href = './detail.html';
-// });
+// Thêm sự kiện click vào từng phần tử
+elements.forEach(element => {
+  element.addEventListener('click', () => {
+    // Lấy productId từ thuộc tính data
+    const productId = element.dataset.productId;
+    console.log(productId);
+
+    // Lưu productId vào Local Storage
+    localStorage.setItem('productId', productId);
+
+    // Chuyển hướng đến trang chi tiết
+    window.location.href = '/src/components/pages/detail.html';
+  });
+});
