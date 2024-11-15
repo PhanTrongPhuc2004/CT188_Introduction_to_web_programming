c70fa4de4f997970401ecb598c65f578bf5192a4
PhanTrongPhuc <phantrongphuc12004@gmail.com>
2024-11-15 11:57:58 +0700

    config header and footer for pages

--- src/components/common/footer.js
+++ src/components/common/footer.js
@@ -10,7 +10,7 @@ const footerHTML = `
         <script src="../../../jquery-3.7.1/jquery-3.7.1.min.js"></script>
         <script src="../../../bootstrap-5.0.2-dist/js/bootstrap.bundle.min.js"></script>
         <!-- <link rel="stylesheet" href="../../css/styles.css"> -->
-        <link rel="stylesheet" href="../../css/components/footer.css">
+        <link rel="stylesheet" href="./src/css/components/footer.css">
         <base href="/">
     </head>
 
