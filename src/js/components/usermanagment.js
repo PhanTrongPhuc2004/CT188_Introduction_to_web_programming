972ea3af2dc5b08c9a47a6fc107ac3f1c4df1823
Lam-Thanh-Thanh <thanhb2203529@student.ctu.edu.vn>
2024-11-15 20:11:23 +0700

    updata userManagement.html, user.js

---
+++ src/js/components/usermanagment.js
@@ -0,0 +1,6 @@
+const headerusermanaElement = document.getElementById('header');
+headerusermanaElement.innerHTML = header_companyHTML;
+
+// Chèn phần footer từ footer.js vào các trang
+const footerusermanaElement = document.getElementById('footer');
+footerusermanaElement.innerHTML = footerHTML;
